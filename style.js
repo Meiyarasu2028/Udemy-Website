cd04a21422fc6cacde073c97055e0f1bb3036745
Meiyarasu <meiysrn06@gmail.com>
2025-08-07 01:30:27 -0700

    updated all Files

---
+++ style.js
@@ -0,0 +1,18 @@
+var sidenav = document.querySelector(".overlay")
+
+function shownavbar(){
+
+    sidenav.style.left="0"
+}
+
+function closenavbar(){
+    sidenav.style.left="-80%"
+}
+
+
+
+var offerbox = document.querySelector(".offer__box")
+
+function offerboxclose(){
+    offerbox.style.display="none"
+}
